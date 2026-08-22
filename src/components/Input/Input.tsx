21a0b87ad96d6d2d6892885213d31f3b1fd73f00
chirag jain <jchirag0610@gmail.com>
2026-08-22 21:38:55 +0530

    some error related to type resolved by adding omit and type

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -33,13 +33,17 @@ const inputVariants = cva(
 );
 
 export interface InputProps
-  extends React.InputHTMLAttributes<HTMLInputElement>,
-    VariantProps<typeof inputVariants> {
+  extends Omit<
+    React.InputHTMLAttributes<HTMLInputElement>,
+    "disabled" | "size"
+  >,
+  Omit<VariantProps<typeof inputVariants>, "disabled" | "size"> {
   label?: string;
   hint?: string;
   error?: string;
   id?: string;
   size?: "sm" | "md" | "lg";
+  disabled?: boolean;
 }
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
